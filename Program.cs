bcb5c2edc02eca2fe5c8f96527337af2add7316d
Beratdmrbs <beratdmrbs61@gmail.com>
2024-10-21 21:44:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Lütfen dört sayı girin:");
+
+Console.Write("1. Sayı: ");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2. Sayı: ");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("3. Sayı: ");
+int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("4. Sayı: ");
+int sayi4 = Convert.ToInt32(Console.ReadLine());
+
+int toplam = sayi1 + sayi2 + sayi3 + sayi4;
+int carpim = sayi1 * sayi2 * sayi3 * sayi4;
+
+Console.WriteLine($"Toplam: {toplam}");
+Console.WriteLine($"Çarpım: {carpim}");
